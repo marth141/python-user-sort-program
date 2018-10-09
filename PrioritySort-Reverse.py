b2ac95f76fd1da2cf5d558aa2396315751f3456f
marth141 <ncasados@live.com>
2018-10-09 15:34:31 -0600

    Got a nicer loop going here

---
+++ PrioritySort-Reverse.py
@@ -0,0 +1,98 @@
+import random
+
+TO_PRIORITIZE = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+SORTED_PRIORITIES = []
+
+
+def prioritize(to_prioritize, sorted_priorities):
+    print('=== prioritize Inputs ===')
+    print('input: ' + str(to_prioritize))
+    print('input: ' + str(sorted_priorities))
+
+    # Get positions
+    positions = Positions(to_prioritize, sorted_priorities)
+    compare(to_prioritize, sorted_priorities, positions)
+
+
+def compare(to_prioritize, sorted_priorities, positions):
+    # Compare to prioritize 1 and 2
+    to_compare_a = to_prioritize[1]
+    to_compare_b = to_prioritize[0]
+    sorted_priorities = seed_sorted_list(to_compare_a, to_compare_b, positions, sorted_priorities)
+    sort_the_rest(to_prioritize, sorted_priorities)
+
+
+def sort_the_rest(to_prioritize, sorted_priorities):
+    print(sorted_priorities)
+    positions = Positions(to_prioritize, sorted_priorities)
+    i = 2
+    while i < positions.unsorted_priorities.end:
+        print(to_prioritize[i])
+        j = 0
+        while j < positions.sorted_priorities.end:
+            print(sorted_priorities[j])
+            j += 1
+        i += 1
+
+
+def seed_sorted_list(to_compare_a, to_compare_b, positions, sorted_priorities):
+    compare_message(to_compare_a, to_compare_b)
+    response = get_input()
+    input_message(response)
+    if response == 'n':
+        if (positions.sorted_priorities.end == positions.sorted_priorities.median):
+            sorted_priorities.insert(positions.sorted_priorities.end, to_compare_a)
+            sorted_priorities.insert(positions.sorted_priorities.end, to_compare_b)
+            return sorted_priorities
+    elif response == 'y':
+        if (positions.sorted_priorities.end == positions.sorted_priorities.median):
+            sorted_priorities.insert(positions.sorted_priorities.end, to_compare_b)
+            sorted_priorities.insert(positions.sorted_priorities.end, to_compare_a)
+            return sorted_priorities
+
+
+class Position:
+    def __init__(self, array_to_get):
+        array_length = len(array_to_get)
+        self.start = 0
+        self.median = int(array_length / 2)
+        self.end = array_length
+
+
+class Positions:
+    def __init__(self, to_sort_array, end_array):
+        self.unsorted_priorities = Position(to_sort_array)
+        self.sorted_priorities = Position(end_array)
+        print('sorted_positions start: ' + str(self.sorted_priorities.start))
+        print('sorted_positions median: ' + str(self.sorted_priorities.median))
+        print('sorted_positions end: ' + str(self.sorted_priorities.end))
+        print('unsorted_positions start: ' + str(self.unsorted_priorities.start))
+        print('unsorted_positions median: ' + str(self.unsorted_priorities.median))
+        print('unsorted_positions end: ' + str(self.unsorted_priorities.end))
+
+
+def yes_no(response):
+    if response == 'y':
+        print('> input: ' + response)
+        return
+    elif response == 'n':
+        print('> input: ' + response)
+        return
+
+
+def compare_message(compare_a, compare_b):
+    print('> Is ' + compare_a + ' more important than ' + compare_b + '? (y/n)')
+    return
+
+
+def input_message(response):
+    print('> input: ' + response)
+    return
+
+
+def get_input():
+    response = 'n'
+    return response
+
+
+prioritize(TO_PRIORITIZE, SORTED_PRIORITIES)
